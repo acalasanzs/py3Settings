e11be54333f1495cc407b52859c99e685a50b259
Albert Calasanz <acalasans@gmail.com>
2023-07-15 19:41:10 +0200

    LICENSE

--- src/py3Settings/__init__.py
+++ src/py3Settings/__init__.py
@@ -1,5 +1,10 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Albert Calasanz Sallen. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
 from .main import AppSettings, Option, Attribute, getWithAttr, addFormatSupport, showFileDefs, printObjProps
 from . import proxy
 # from . import utils
 from . import file
 # No hay recursividad
+__version__ = "2.1.0"
