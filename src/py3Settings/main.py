672524fba3999ecc475c6a1b40a63593b2e8621a
Albert Calasanz <acalasanzs@gmail.com>
2023-10-27 12:53:18 +0200

    recursiveling

--- src/py3Settings/main.py
+++ src/py3Settings/main.py
@@ -26,15 +26,28 @@ class Attribute:
         We then create an Option called my_option with an option name of "my_option", an option ID of "option_id", and append the attr1 and attr2 attributes to it.
 
         This Option now has two attributes that can be used to validate and process data in the AppSettings class. For example, if we have a settings file with a "my_option" section that contains "attr1" and "attr2" keys, we can use the validateAll method of the AppSettings class to validate the values of these keys using the attr1 and attr2 attributes of the my_option option.
+    parameters:
+        self: A reference to the instance of the class being created.
+        
+        attr: A string that represents the name of the attribute.
+        
+        typ: An optional parameter that specifies the type of the attribute. If this parameter is not provided, the validate parameter must be a callable that takes an object and returns a boolean value indicating whether the object is valid.
+        
+        validate: An optional parameter that specifies a callable that takes an object and returns a boolean value indicating whether the object is valid. If this parameter is not provided, the typ parameter must be a type object.
+        
+        default: An optional parameter that specifies the default value of the attribute.
+        
+        getter -> get: An optional parameter that specifies a callable that returns the value of the attribute. This is useful if the attribute is computed or if it needs to be retrieved from a different location.
+        In summary, the Attribute class is used to define an attribute with a name, a type or validation function, a default value, and an optional getter function. The typ and validate parameters are mutually exclusive, and at least one of them must be provided. If typ is provided, the validate function is automatically generated to check if the value is an instance of typ. If validate is provided, it is used to validate the value instead.
     """
 
     def __init__(
         self,
         attr: str,
-        typ: Any | None = None,
+        typ: type | None = None,
         validate: Callable[[object], bool] | None = None,
         default: bool = False,
-        getter: Callable[[], Any] = None,
+        getter: Callable[[object], object] = None,
     ):
         self.attr = attr
         if typ is None and validate is Callable[[object], bool]:
@@ -49,6 +62,8 @@ class Attribute:
         else:
             raise SystemExit("No type!")
         self.default = default
+    def get(object: object):
+        return object
 
 
 class Option:
@@ -84,7 +99,17 @@ def addFormatSupport(Handler: Handler):
 
 def showFileDefs():
     return file
+class InAttribute:
 
+    def __init__(self, attr: str, options: List[Attribute], default: bool = False, validateAll: bool = True, getter: Callable[[], Any] = None):
+        self.attr = attr
+        if getter is not None:
+            self.get = getter
+        self.default = default
+        self.options = options
+
+    def default(self) -> dict:
+        return {option.attr: option.default for option in self.options}
 
 class AppSettings(Mapping):
     def __init__(self, options: List[Option]):
@@ -196,16 +221,21 @@ class AppSettings(Mapping):
                     self.defaults[option.name] = statement[option.default.attr]
 
     def getSetting(self, name: str, attr: str | None):
+        def getSettingClosure(value):
+            if attr in self.options:
+                return self.options[attr].get(value)
+            else:
+                return self.defaults[attr].get(value)
         if attr is None:
-            return self.defaults[name]
+            return getSettingClosure(self.defaults[name])
         try:
-            return self.dict[name][attr]
+            return getSettingClosure(self.dict[name][attr])
         except:
             try:
                 option = next((option for option in self.options if option.name == name), None)
                 if option is None:
                     raise KeyError(f"Option {name} not found in settings")
-                return option.default.default
+                return getSettingClosure(option.default.default)
             except KeyError:
                 raise KeyError(f"Attribute {attr} not found in settings")
 
