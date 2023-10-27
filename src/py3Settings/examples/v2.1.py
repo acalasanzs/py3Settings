876afa75c08c5ae04f3f005fd39617d4d023a4dc
Albert Calasanz 2 <acalasanzs@gmail.com>
2023-10-27 11:15:57 +0200

    A

--- src/py3Settings/examples/v2.1.py
+++ src/py3Settings/examples/v2.1.py
@@ -1,4 +1,7 @@
+import click
+from colorama import Fore, Style
 from init import *
+
 # Define an attribute for the option
 """TODO
     *1. Add support for multiple conditions asides lambda, like int determiantors, etc.
@@ -23,14 +26,44 @@ app_settings = AppSettings(options)
 app_settings.validateAll()
 
 # Get the value of an attribute for an option
-attr1_value = app_settings.getSetting("my_option", "attr1")
-print(attr1_value)
+@click.command()
+@click.option('--option', prompt='Enter option name', help='Name of the option')
+@click.option('--attribute', prompt='Enter attribute name', help='Name of the attribute')
+def get_setting(option, attribute):
+    attr_value = app_settings.getSetting(option, attribute)
+    click.echo(Fore.GREEN + f"{attribute} value for {option}: {attr_value}" + Style.RESET_ALL)
 
 # Get all settings data
-settings_data = app_settings.getSettings()
+@click.command()
+def get_settings():
+    settings_data = app_settings.getSettings()
+    click.echo(Fore.GREEN + "All settings data:" + Style.RESET_ALL)
+    click.echo(settings_data)
 
 # Get default settings data
-default_settings_data = app_settings.getDefaultSettings()
+@click.command()
+def get_default_settings():
+    default_settings_data = app_settings.getDefaultSettings()
+    click.echo(Fore.GREEN + "Default settings data:" + Style.RESET_ALL)
+    click.echo(default_settings_data)
 
 # Save settings data to a file
-app_settings.saveFile(r"C:\Users\acses\Documents\py3Settings\tests\settings.json")
+@click.command()
+@click.option('--file', prompt='Enter file path', help='Path to the file to save settings data')
+def save_settings(file):
+    app_settings.saveFile(file)
+    click.echo(Fore.GREEN + "Settings data saved to file successfully!" + Style.RESET_ALL)
+
+# Create a group of commands
+@click.group()
+def cli():
+    pass
+
+# Add commands to the group
+cli.add_command(get_setting)
+cli.add_command(get_settings)
+cli.add_command(get_default_settings)
+cli.add_command(save_settings)
+
+if __name__ == '__main__':
+    cli()
